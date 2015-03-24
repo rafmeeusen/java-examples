e8adc14c117fc0087b9136b06bb225a2b3ec08da
Raf Meeusen <raf.meeusen@telit.com>
2015-03-24 11:52:07 +0100

    classloader example: printing some well known classloaders

--- crypto/Examples/src/net/meeusen/examples/classloading/Main.java
+++ crypto/Examples/src/net/meeusen/examples/classloading/Main.java
@@ -14,22 +14,37 @@ public class Main {
 		System.out.println("Parent classloader of extension class loader should be bootstrap class loader.");
 		System.out.println();
 		
+		System.out.println("1. Let's print class loader delegation hierarchy starting from this Main class.");
 		ClassLoader thismaincl = Main.class.getClassLoader(); 
-		System.out.println("Classloader of this Main class: " + thismaincl); 
-		
+		System.out.println("Classloader of this Main class: " + thismaincl); 		
 		ClassLoader appclassloader = ClassLoader.getSystemClassLoader(); 
 		if (appclassloader.equals(thismaincl)) {
 			System.out.println("\tOK, method ClassLoader.getSystemClassLoader() returned same class loader. ");
 		} else {
 			System.out.println("\tMmmm. Not right. Method ClassLoader.getSystemClassLoader() returned another class loader. ");
 		}
-		
 		ClassLoader parentcl = thismaincl.getParent(); 
 		System.out.println("and its parent: " + parentcl);
 		ClassLoader grandparentcl = parentcl.getParent(); 
-		System.out.println("and its grandparent: " + grandparentcl); 
-		
+		System.out.println("and its grandparent: " + grandparentcl); 		
 		System.out.println("Bootstrap classloader (grandparent) is often null since usually implemented in native code in VM.");
+		System.out.println();
+		
+		System.out.println("2. Let's print the class loader of some well-known classes/interfaces.");
+		Class<?> classes[] = new Class[] {
+				com.sun.crypto.provider.SunJCE.class,				
+				com.sun.nio.zipfs.JarFileSystemProvider.class,
+				java.io.File.class, 
+				java.security.Provider.class, 
+				javax.crypto.SecretKey.class, 
+				javax.crypto.Cipher.class, 
+				Object.class, 
+				sun.security.ec.SunEC.class, 
+				}; 		
+		for ( Class<?> c: classes ) {
+			System.out.println(c + " <---> " + c.getClassLoader());	
+		}
+		
 
 		
 	}
