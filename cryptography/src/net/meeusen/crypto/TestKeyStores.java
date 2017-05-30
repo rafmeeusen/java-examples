22de340ba4f49e765edf68f042686549b20bc406
Raf Meeusen <raf.meeusen@telit.com>
2017-05-30 10:36:45 +0200

    cleanup of keystore test

--- cryptography/src/net/meeusen/crypto/TestKeyStores.java
+++ cryptography/src/net/meeusen/crypto/TestKeyStores.java
@@ -2,10 +2,8 @@ package net.meeusen.crypto;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.security.Key;
 import java.security.KeyStore;
-import java.security.KeyStore.SecretKeyEntry;
 import java.security.NoSuchAlgorithmException;
 import java.security.NoSuchProviderException;
 import java.security.cert.Certificate;
@@ -16,111 +14,81 @@ import javax.crypto.SecretKey;
 
 import net.meeusen.util.ByteString;
 
-
 public class TestKeyStores {
 
-    private static String pwd_entry = "our test entry password"; 
-    private static SecretKey testSecKey = null; 
+    private static String pwd_entry = "our test entry password";
+    private static SecretKey testSecKey = null;
     private static Certificate testCert = null;
+
+    static void doTest(KeyStore ks) throws Exception {
+
+        System.out.println("Start of test for KeyStore type " + ks.getType() + " of provider " + ks.getProvider());
+        ks.load(null, null);
+        trySetKeyEntry(ks, getOurTestSecretKey());
+        trySetCertEntry(ks, getOurTestCertificate());
+    }
 
     public static void main(String[] args) throws Exception {
         System.out.println("Testing capabilities of different keystore types");
 
-        System.out.println("Default type keystore: " + KeyStore.getDefaultType()); 
-        KeyStore ks_jceks = KeyStore.getInstance("jceks"); 
-        KeyStore ks_jks = KeyStore.getInstance("jks");
-        KeyStore ks_pkcs12 = KeyStore.getInstance("PKCS12"); 
+        KeyStore[] keyStoresToTest = new KeyStore[] { KeyStore.getInstance("jceks"), KeyStore.getInstance("jks"),
+                KeyStore.getInstance("PKCS12"), };
 
-        ks_jks.load(null,null);
-        ks_jceks.load(null,null);
-        ks_pkcs12.load(null,null);  
+        for (KeyStore ks : keyStoresToTest) {
+            doTest(ks);
+        }
 
-
-        //System.out.println( "Secret key used here:"+ Helpers.bytes2HexString(getOurTestSecretKey().getEncoded()) );
-        System.out.println("Trying setKeyEntry() with SecretKey for each type:"); 
-        trySetKeyEntry(ks_jks, getOurTestSecretKey());
-        trySetKeyEntry(ks_jceks, getOurTestSecretKey());
-        trySetKeyEntry(ks_pkcs12, getOurTestSecretKey());
-        ////////////////////////////////////////////
-        System.out.println("Trying setCertificateEntry() for each type:"); 
-        trySetCertEntry(ks_jks, getOurTestCertificate()); 
-        trySetCertEntry(ks_jceks, getOurTestCertificate());
-        trySetCertEntry(ks_pkcs12, getOurTestCertificate());
-        //////////////////////////////////////////////        
-
-        //ks_jks.store(new FileOutputStream("ks_jks.ks"), null);
-        ks_jks.store(new FileOutputStream("ks_jceks.ks"), null);
-        //ks_jks.store(new FileOutputStream("ks_pkcs12.ks"), null);
-        
-        
-        System.out.println("certificate used:");
-        System.out.println(getOurTestCertificate());
-        
-//        KeyStore.SecretKeyEntry mysk = new KeyStore.SecretKeyEntry(getOurTestSecretKey());
-//        KeyStore.ProtectionParameter protParam = new KeyStore.PasswordProtection("blablabla123".toCharArray()); 
-//        ks_jceks.setEntry("seckey1", mysk, protParam);
-
-//        System.out.println("size: "+ks_jks.size());
-//        ks_jceks.store(new FileOutputStream(new File("test.jks")), "".toCharArray());
-//
-//        KeyStore.SecretKeyEntry entryout = (SecretKeyEntry) ks_jceks.getEntry("seckey1", protParam);
-//        SecretKey keyout = entryout.getSecretKey(); 
-//        System.out.println(keyout.getAlgorithm());
-//        System.out.println(keyout.getFormat());
-//        byte[] keybytes = keyout.getEncoded();
-//        System.out.println( Helpers.bytes2HexString(keybytes));
     }
-
 
     private static void trySetKeyEntry(KeyStore ourks, Key ourkey) {
         String message = "Storing " + ourkey.getClass() + " in " + ourks.getType() + " ";
         System.out.print(message);
-        try {		
-            String alias = getRandomAlias(); 
+        try {
+            String alias = getRandomAlias();
             ourks.setKeyEntry(alias, ourkey, pwd_entry.toCharArray(), null);
-            System.out.println("SUCCEEDED for alias "+alias);
+            System.out.println("SUCCEEDED for alias " + alias);
         } catch (Exception e) {
-            System.out.println("FAILED. " + e.getMessage()); 
+            System.out.println("FAILED. " + e.getMessage());
         }
     }
 
     private static void trySetCertEntry(KeyStore ourks, Certificate c) {
         String message = "Storing " + c.getClass() + " in " + ourks.getType() + " ";
         System.out.print(message);
-        try {	
-            String alias = getRandomAlias(); 
+        try {
+            String alias = getRandomAlias();
             ourks.setCertificateEntry(alias, c);
-            System.out.println("SUCCEEDED for alias "+alias);
+            System.out.println("SUCCEEDED for alias " + alias);
         } catch (Exception e) {
-            System.out.println("FAILED. "+ e.getMessage()); 
+            System.out.println("FAILED. " + e.getMessage());
         }
     }
 
     private static String getRandomAlias() {
-        Random rng = new Random(); 
-        byte[] ranbytes = new byte[8]; 
+        Random rng = new Random();
+        byte[] ranbytes = new byte[8];
         rng.nextBytes(ranbytes);
         return new ByteString(ranbytes).toHexString();
     }
 
     private static SecretKey getOurTestSecretKey() throws NoSuchAlgorithmException, NoSuchProviderException {
-        if ( testSecKey == null ) {
+        if (testSecKey == null) {
             testSecKey = KeyGenerator.getInstance("AES").generateKey();
         }
-        return testSecKey; 
+        return testSecKey;
     }
 
     private static Certificate getOurTestCertificate() throws Exception {
-        if ( testCert == null ) {
-            String basepath = System.getProperty("java.home");      
-            File cacertfile = new File (basepath+"/lib/security/cacerts"); 
-            if ( ! cacertfile.exists()) {
+        if (testCert == null) {
+            String basepath = System.getProperty("java.home");
+            File cacertfile = new File(basepath + "/lib/security/cacerts");
+            if (!cacertfile.exists()) {
                 throw new Exception("mmm");
-            }       
-            KeyStore systemTrustStore = KeyStore.getInstance(KeyStore.getDefaultType()); 
+            }
+            KeyStore systemTrustStore = KeyStore.getInstance(KeyStore.getDefaultType());
             systemTrustStore.load(new FileInputStream(cacertfile), "changeit".toCharArray());
             String firstEntryAlias = systemTrustStore.aliases().nextElement();
-            testCert = systemTrustStore.getCertificate(firstEntryAlias);	        
+            testCert = systemTrustStore.getCertificate(firstEntryAlias);
         }
         return testCert;
     }
